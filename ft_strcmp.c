8c2f4a7a598d706e74fc845ca0c7dc8a504cd4bc
Kaelin LEES <klees@c3r6s1.wethinkcode.co.za>
2019-05-24 08:17:28 +0200

    Libft

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,16 +6,24 @@
 /*   By: klees <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 07:32:44 by klees             #+#    #+#             */
-/*   Updated: 2019/05/23 17:56:10 by klees            ###   ########.fr       */
+/*   Updated: 2019/05/24 07:59:34 by klees            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	int x;
+	int				x;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
 
 	x = 0;
-	while (s1[x] != '\0' && s2[x] != '\0' && s1[x] == s2[x])
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
+	while (ss1[x] != '\0' && ss2[x] != '\0')
+	{
+		if (ss1[x] != ss2[x])
+			return (ss1[x] - ss2[x]);
 		x++;
-	return (s1[x] - s2[x]);
+	}
+	return (ss1[x] - ss2[x]);
 }
